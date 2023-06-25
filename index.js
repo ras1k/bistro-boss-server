bf3ad434eb01f331d777013d13272c8646664921
Rasik Abdullah <rasikabdullah@gmail.com>
2023-06-26 03:00:10 +0600

    delete api added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -57,6 +57,13 @@ async function run() {
       const item = req.body;
       console.log(item)
       const result = await cartCollection.insertOne(item);
+      res.send(result)
+    })
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result)
     })
 
