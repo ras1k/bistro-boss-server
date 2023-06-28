c683217e39f808b64966a7326938dbf7ece51f85
Rasik Abdullah <rasikabdullah@gmail.com>
2023-06-29 01:25:26 +0600

    user deleted

--- index.js
+++ index.js
@@ -69,6 +69,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result)
+    })
+
     //menu
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
