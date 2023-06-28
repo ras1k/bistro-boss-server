9d0318083b50428c51a48caa6dd7b4871fe1b295
Rasik Abdullah <rasikabdullah@gmail.com>
2023-06-28 18:21:12 +0600

    jwt added

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ async function run() {
     const reviewCollection = client.db('bistroDB').collection('reviews');
     const cartCollection = client.db('bistroDB').collection('carts');
 
+    //jwt
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token })
+    })
+
     //users
     app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
@@ -50,9 +57,9 @@ async function run() {
       res.send(result)
     })
 
-    app.patch('/users/admin/:id', async(req, res) => {
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
           role: 'admin'
