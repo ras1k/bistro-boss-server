3d42727b6c35cb0351ed2f1c8a15cc6703a161e0
Rasik Abdullah <rasikabdullah@gmail.com>
2023-07-14 03:16:37 +0600

    some code added

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ async function run() {
     })
 
     //create payment
-    app.post('/create-payment-intent', async (req, res) => {
+    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
       const { price } = req.body;
       const amount = price * 100;
       const paymentIntent = await stripe.paymentIntents.create({
