beb297e60c00f6e9fe13c268d2e5166bd98f0095
Rasik Abdullah <rasikabdullah@gmail.com>
2023-07-15 02:47:06 +0600

    payment errors fixed

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
+
+// const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.PAYMENT_SECRET_KEY);
+
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -181,15 +185,16 @@ async function run() {
     //create payment
     app.post('/create-payment-intent', verifyJWT, async (req, res) => {
       const { price } = req.body;
-      const amount = price * 100;
+      const amount = parseInt(price * 100);
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
-        currency: "usd",
+        currency: 'usd',
         payment_method_types: ['card']
       });
+
       res.send({
-        clientSecret: paymentIntent.client_secret,
-      });
+        clientSecret: paymentIntent.client_secret
+      })
     })
 
     // Send a ping to confirm a successful connection
