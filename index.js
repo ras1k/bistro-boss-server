9fd98e662c27504e0911bbe04293ee4d1c982d09
Rasik Abdullah <rasikabdullah@gmail.com>
2023-07-17 19:07:20 +0600

    admin stats api added"

--- index.js
+++ index.js
@@ -208,6 +208,22 @@ async function run() {
       res.send({ result, deleteResult })
     })
 
+    app.get('/admin-stats', verifyJWT, verifyAdmin, async (req, res) => {
+      const users = await usersCollection.estimatedDocumentCount();
+      const products = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce((sum, payment) => sum + payment.price, 0)
+
+      res.send({
+        revenue,
+        users,
+        products,
+        orders
+      })
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
